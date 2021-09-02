df6b5f11ca5dd3b23eb10c8edd08f08e00d97d21
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2021-09-02 03:55:00 +0000

    Add files via upload

--- colab.js
+++ colab.js
@@ -1,7 +1,7 @@
 (function(){
   try{
-        console.log(“Working”);
-        document.querySelector(“colab-connect-button”).shadowRoot.getElementById(‘connect’).click();
+        console.log('Working');
+        document.querySelector('colab-connect-button').shadowRoot.getElementById('connect').click();
     } catch(e) {
         console.log(e);
     }
